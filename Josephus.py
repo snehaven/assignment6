fe13ec2bdda53f74b65b3e2ab6b62e4975f72a1f
snehaven <65678267+snehaven@users.noreply.github.com>
2022-03-08 20:54:13 -0600

    Update Josephus.py

--- Josephus.py
+++ Josephus.py
@@ -6,8 +6,16 @@ class Link(object):
         self.next = next
         self.before = before
 
+  # Delete the nth Link starting from the Link start
+  # Return the data of the deleted Link AND return the
+  # next Link after the deleted Link in that order
     def __str__(self):
-        return str(self.before) + " --> " + str(self.data) + " --> " + str(self.next)
+        stringrep = str(self.data) + " --> " + str(self.next.data) + " --> "
+        current = self.next
+        while (current.next.data is not self.data):
+            stringrep += str(current.next.data) + " --> "
+            current = current.next
+        return stringrep
 
 
 class CircularList(object):
@@ -20,25 +28,31 @@ class CircularList(object):
       newLink = Link(data)
       if(self.first is None):
           self.first = newLink
+          self.first.next = self.first
       else:
           current = self.first
-          while(current.next is not None):
+
+          while(current.next is not self.first):
               current = current.next
           current.next = newLink
           before = current
           current = current.next
           current.before = before
+          current.next = self.first
 
   # Find the Link with the given data (value)
   # or return None if the data is not there
   def find ( self, data ):
-      current = self.first
+    current = self.first
     if (current == None):
       return None
 
-    # searcg and find the position of the given data, the get the link if.
+    if (data == self.first.data):
+      return self.first
+
+    # search and find the position of the given data, the get the link if.
     while (current.data != data):
-      if (current.next == None):
+      if (current.next == self.first.data):
         return None
       else:
         current = current.next
@@ -47,67 +61,33 @@ class CircularList(object):
 
   # Delete a Link with a given data (value) and return the Link
   # or return None if the data is not there
-  def delete ( self, data ):
-      current = self.first
-    if current.next != None:
-      if current.data == data:
-        current.next.previous = None
-        self.first = current.next
-        current.next = None
-        return current
-      else:
-        while current.next != None:
-          if current.data == data:
-            break
-          else:
-            current = current.next
-        if current.next != None:   
-          current.previous.next = current.next
-          current.next.previous = current.previous
-          current.previous = None
-          current.next = None
-        else:
-          current.previous.next = None
-          current.previous = None
-        return current
-    
-    if current == None:
-      return None
 
-  # Delete the nth Link starting from the Link start
-  # Return the data of the deleted Link AND return the
-  # next Link after the deleted Link in that order
+  def delete ( self, data ):
+    need_to_delete = self.find(data)
+    if (need_to_delete is None):
+        return None
+
+    before = need_to_delete.before
+    after = need_to_delete.next
+    before.next = after
+    after.before = before
+
+    return need_to_delete
+
+
   def delete_after ( self, start, n ):
-      current = start
-    print( start.next )
-    
-    for j in range ( n ):
-        if current.next != None:
+        current = start
+        for i in range(n - 1):
             current = current.next
-    if current.next != None:
-        current.before.next = current.next
-        current.next.before = current.before
-        current.before = None
-        current.next = None
+
+        self.delete(current.data)
         return current.data, current.next
-    else:
-        current.before.next = None
-        current.before = None
-        return current.data, None
 
   # Return a string representation of a Circular List
   # The format of the string will be the same as the __str__
   # format for normal Python lists
   def __str__ ( self ):
-    strng =""
-    current = self.first
-    strng += str(current.data) + ' <-> '
-    current = current.next
-    while(current.next != self.first and current.next != None):
-      strng += str(current.data) + ' <-> '
-      current = current.next
-    strng += str(current.data) + ' <-> '
-    return strng
+      return str(self.first)
 
 def main():
   # read number of soldiers
@@ -125,17 +105,21 @@ def main():
   line = line.strip()
   elim_num = int (line)
 
-  # your code
-  CirularList = CircularList()
+  # Create a circular list
   circularList = CircularList()
-  for i in range ( num_soldiers ):
-        circularList.insert(i)
+
+  #Populate circular list
+  for i in range (1, num_soldiers + 1 ):
+      circularList.insert(i)
+
+  #Delete every nth soldier
   start = circularList.find( start_count )
 
-  while num_soldiers > 1:
+  while num_soldiers >= 1:
         eliminated, start = circularList.delete_after ( start, elim_num )
         num_soldiers -= 1
-        print( eliminated )
+        print( eliminated)
 
 if __name__ == "__main__":
   main()
+
